44a2ecaa75f33288fd716f1c8800dbea6776353d
jeffpohlmeyer <jpohlmeyer@arqu.co>
2023-11-10 10:55:28 -0500

    adding `render` method to helpers.go

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/go-playground/form/v4"
+	"net/http"
+	"time"
+)
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
@@ -18,4 +25,68 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+	// Retrieve the appropriate template set from the cache based on the page
+	// name (like 'home.tmpl'). If no entry exists in the cache with the
+	// provided name, then create a new error and call the serverError() helper
+	// method that we made earlier and return
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	// Initialize a new buffer
+	buf := new(bytes.Buffer)
+
+	// Write the template to the buffer, instead of straight to the http.ResponseWriter. If there's an error, call our
+	// serverError() helper and then return
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	// If the template is written to the buffer without any errors, we are safe
+	// to go ahead and write the HTTP status code to http.ResponseWriter
+	w.WriteHeader(status)
+
+	// Write the contents of the buffer to the http.ResponseWriter. Note: this
+	// is another time where we pass our http.ResponseWriter to a function that
+	// takes an io.Writer
+	buf.WriteTo(w)
+}
+
+// Create a newTemplateData() helper, which returns a pointer to a templateData struct initialized with any common data.
+// Note that we're not using the *http.Request parameter here at the moment, but we will do later
+func (app *application) newTemplateData(r *http.Request) templateData {
+	return templateData{CurrentYear: time.Now().Year()}
+}
+
+// Create a new decodePostForm() helper method. The second parameter here ,dst, is teh target destination into which we
+// want to decode the form data
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	// Call ParseForm() on the request, in the same way that we did in our redirectCreate handler
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	// Call Decode() on our decoder instance, passing the target destination as the first parameter
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		// If we try to use an invalid target destination, the Decode() method will return an error with the type
+		// *form.InvalidDecoderError. We use errors.As() to check for this and raise a panic rather than returning the
+		// error
+		var invalidDecoderError *form.InvalidDecoderError
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+	return nil
 }
