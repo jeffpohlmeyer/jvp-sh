676d3460a5ad3ee6c01add2ae4d8278f9fc26615
jeffpohlmeyer <jpohlmeyer@arqu.co>
2023-11-10 10:56:09 -0500

    utilizing new `render` method in handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,78 +4,75 @@ import (
 	"errors"
 	"fmt"
 	"github.com/jeffpohlmeyer/jvp-sh/internal/models"
-	"html/template"
-	"log"
+	"github.com/jeffpohlmeyer/jvp-sh/internal/validator"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
+type urlCreateForm struct {
+	Url                 string `form:"url"`
+	validator.Validator `form:"-"`
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/partials/nav.html",
-		"./ui/html/pages/home.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
+	// Call the newTemplateData() helper to get a templateData struct containing the 'default' data (which for now
+	// just contains the current year)
+	data := app.newTemplateData(r)
+	data.Form = urlCreateForm{}
+	app.render(w, r, http.StatusOK, "home.html", data)
 }
 
 func (app *application) redirectCreate(w http.ResponseWriter, r *http.Request) {
+	// Declare a new empty instance of the urlCreateForm
+	var form urlCreateForm
+
 	// First we call r.ParseForm() which adds any data in POST request bodies
 	// to the r.PostForm map. This also works in the same way for PUT and PATCH
 	// requests. If there are any errors, we use our app.ClientError() helper to
 	// send a 400 Bad Request response to the user.
-	err := r.ParseForm()
+	err := app.decodePostForm(r, &form)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	// Use the r.PostForm.Get() method to retrieve the url from the r.PostForm map
-	// If there is no matching parameter, or the parameter is empty, then the
-	// url variable will be set to the empty string.
-	url := r.PostForm.Get("url")
+	// Because the Validator struct is embedded by the urlCreateForm struct,
+	// we can call CheckField() directly on it to execute our validation checks.
+	// CheckField() will add the provided key and error message to the
+	// FieldErrors map if the check does not evaluate to true. For example, in
+	// the first line here we "check that the form.Url field is not blank". In
+	// the second, we "check that the form.Url field is a valid URL and so on
+	form.CheckField(validator.NotBlank(form.Url), "url", "Url cannot be blank")
+	form.CheckField(validator.IsValidUrl(form.Url), "url", "Url is not valid")
 
-	// If the url field is empty then add a "Url cannot be blank" error message to
-	// the validationErrors map. Notice that we then return the validationErrors
-	// map to the user using our app.badRequest() helper. This will render the
-	// form again along with the validation errors. If there were no errors, then
-	// we just log a message and redirect the user to the relevant page.
-	if url == "" {
-		app.clientError(w, http.StatusBadRequest)
+	// Use the Valid() method to see if any of the checks failed. If they did,
+	// then re-render the template passing in the form in the same way as
+	// before
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "home.html", data)
 		return
 	}
 
 	// Create the new url record in the database using the url model's Insert()
 	// method. If this returns an error, we log the detailed error message and
 	// send a 500 Internal Server Error response to the user.
-	redirect, err := app.urls.Insert(url)
+	redirect, err := app.urls.Insert(form.Url)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/%s+", redirect), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/u/%s+", redirect), http.StatusSeeOther)
 }
 
 func (app *application) redirectShow(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the "shortUrl" parameter from the query string. If none exists,
 	// then we redirect the user to the homepage.
-	shortUrl := r.URL.Query().Get(":shortUrl")
+	params := httprouter.ParamsFromContext(r.Context())
+	shortUrl := params.ByName("shortUrl")
+	app.logger.Info(fmt.Sprintf("shortUrl %s", shortUrl))
 	if shortUrl == "" {
 		app.clientError(w, http.StatusNotFound)
 		return
@@ -85,6 +82,7 @@ func (app *application) redirectShow(w http.ResponseWriter, r *http.Request) {
 	// url based on the shortUrl value. If no matching record is found, we
 	// redirect the user to the homepage.
 	url, err := app.urls.Get(shortUrl)
+	app.logger.Info(fmt.Sprintf("url %s", url.Redirect))
 	if err != nil {
 		if errors.Is(err, models.ErrRecordNotFound) {
 			app.clientError(w, http.StatusNotFound)
@@ -96,21 +94,9 @@ func (app *application) redirectShow(w http.ResponseWriter, r *http.Request) {
 
 	lastChar := shortUrl[len(shortUrl)-1:]
 	if lastChar == "+" {
-		files := []string{
-			"./ui/html/base.html",
-			"./ui/html/partials/nav.html",
-			"./ui/html/pages/home.html",
-		}
-
-		ts, err := template.ParseFiles(files...)
-		if err != nil {
-			app.serverError(w, r, err)
-		}
-
-		err = ts.ExecuteTemplate(w, "base", nil)
-		if err != nil {
-			app.serverError(w, r, err)
-		}
+		data := app.newTemplateData(r)
+		app.render(w, r, http.StatusOK, "shortened.html", data)
 	}
-	http.Redirect(w, r, url.URL, http.StatusPermanentRedirect)
+	app.logger.Info(fmt.Sprintf("url.URL %s", url.Redirect))
+	http.Redirect(w, r, url.Redirect, http.StatusPermanentRedirect)
 }
