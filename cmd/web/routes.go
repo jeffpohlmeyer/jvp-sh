7692608699a4e656772ca1a8fef8b7097cb74823
jeffpohlmeyer <jpohlmeyer@arqu.co>
2023-11-10 10:53:13 -0500

    adding NotFound handling to thˆe router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,12 +9,20 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// Create a handler function which wraps our notFound() helper, and then
+	// assign it as the custom handler for 404 Not Found responses. You can also
+	// set a custom handler for 405 Method Not Allowed responses by setting
+	// router.MethodNotAllowed in the same way too
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodPost, "/", app.redirectCreate)
-	router.HandlerFunc(http.MethodGet, "/:shortUrl", app.redirectShow)
+	router.HandlerFunc(http.MethodGet, "/u/:shortUrl", app.redirectShow)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
